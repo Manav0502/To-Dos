1eb50c67b50d0e9de48fbd7ae47ac818811cb3c4
Manav0502 <manavsurendraagarwal@gmail.com>
2026-09-11 02:09:20 +0530

    Configure root package.json build scripts and backend static path for deployment

--- backend/server/server.js
+++ backend/server/server.js
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../../dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../../', 'dist', 'index.html'));
   });
 }
 
